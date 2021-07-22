718aa15ee94c98c7b756c4de2216b284448331ec
Bishwas Gautam <bgautam@bloominsurance.com>
2021-07-22 17:28:22 -0400

    Use a more reliable way to detect a browser on a mobile device or tablet

--- src/browser.js
+++ src/browser.js
@@ -31,13 +31,27 @@ function getUrlParams() {
  * @type {boolean}
  */
 const isMobile = (() => {
-  if (
-    typeof navigator === "undefined" ||
-    typeof navigator.userAgent !== "string"
-  ) {
-    return false;
+  //ref: https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent
+  var hasTouchScreen = false;
+  if ("maxTouchPoints" in navigator) {
+    hasTouchScreen = navigator.maxTouchPoints > 0;
+  } else if ("msMaxTouchPoints" in navigator) {
+    hasTouchScreen = navigator.msMaxTouchPoints > 0;
+  } else {
+    var mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+    if (mQ && mQ.media === "(pointer:coarse)") {
+      hasTouchScreen = !!mQ.matches;
+    } else if ("orientation" in window) {
+      hasTouchScreen = true; // deprecated, but good fallback
+    } else {
+      // Only as a last resort, fall back to user agent sniffing
+      var UA = navigator.userAgent;
+      hasTouchScreen =
+        /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+        /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
+    }
   }
-  return /Mobile/.test(navigator.userAgent);
+  return hasTouchScreen;
 })();
 
 const isValidFirefoxForScreenshare = () => {
